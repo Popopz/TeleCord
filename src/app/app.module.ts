28c3bd9abef586211ce59d15a746624483ab8678
Popopz <mpcmurphyait@gmail.com>
2021-09-06 10:59:53 +1000

    Added Group, Login, Users Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupComponent } from './group/group.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupComponent,
+    LoginComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
